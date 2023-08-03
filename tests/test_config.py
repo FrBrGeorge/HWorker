3155914b26176f510d0cebafdc0bd6a0a7be915d
Nikita Barabanov <Barabanov.ns@gmail.com>
2023-08-03 11:10:47 +0300

    Change task_name and task_ID in config, new fields

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,15 +10,16 @@ from hworker.config import get_git_directory, get_repos, get_uids, repo_to_uid, 
 
 def test_config():
     """"""
-    # TODO: check all fields
-    test_config_name, test_final_config_name = map(lambda s: "test_" + s, (_user_config_name, _final_config_name))
-    get_final_config(default_config=_default_config_name,
-                     user_config=test_config_name,
-                     final_config=test_final_config_name)
+    # TODO: check all fields, add tests configs
+    # test_config_name, test_final_config_name = map(lambda s: "test_" + s, (_user_config_name, _final_config_name))
+    get_final_config(default_config=_default_config_name,)
+                     # user_config=test_config_name,
+                     # final_config=test_final_config_name)
     assert get_git_directory() == "/tmp/hworker_git"
     assert get_repos() == ["repo", ]
-    assert get_uids() == ["username", ]
-    assert uid_to_repo("username") == "repo"
-    assert repo_to_uid("repo") == "username"
-    os.remove(test_config_name)
-    os.remove(test_final_config_name)
+    assert get_uids() == ["user_ID", ]
+    assert uid_to_repo("user_ID") == "repo"
+    assert repo_to_uid("repo") == "user_ID"
+    # TODO: make path relative
+    os.remove(os.path.join("hworker", "config", _user_config_name))
+    os.remove(os.path.join("hworker", "config", _final_config_name))
