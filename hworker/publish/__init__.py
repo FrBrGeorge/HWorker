dc3d920198bd7489420e393d96efaa79d6048fa8
Veniamin Arefev <veniamin.arefev@mail.ru>
2023-08-15 17:25:15 +0600

    Finally, publish open beta test. Reworked depot according to qualifiers working schema. Now publish can publish multiple entries of UserScore per user_id and TaskScore per task_id

--- hworker/publish/__init__.py
+++ hworker/publish/__init__.py
@@ -2,8 +2,10 @@ import os
 
 
 from .app import app
+from ..log import get_logger
 
 
 def run_server():
+    get_logger(__name__).info("Publish initialized...")
     if "DEBUG_ENVIRONMENT" in os.environ:
         app.run(debug=True, use_debugger=True, use_reloader=False)
