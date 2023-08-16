522edec9ffa345cde32917c1c80a46e957ce9614
Nikita Barabanov <Barabanov.ns@gmail.com>
2023-08-16 17:19:48 +0300

    Change get_checks homework parsing, fix tests

--- hworker/make/__init__.py
+++ hworker/make/__init__.py
@@ -1,7 +1,12 @@
-import os
-
 from ..depot.objects import Homework, Check, Solution, CheckCategoryEnum
-from ..config import get_runtime_suffix, get_validate_suffix, get_remote_name, get_prog_name, get_task_info
+from ..config import (
+    get_runtime_suffix,
+    get_validate_suffix,
+    get_check_name,
+    get_remote_name,
+    get_prog_name,
+    get_task_info,
+)
 
 
 def get_checks(hw: Homework) -> list[Check]:
@@ -11,36 +16,33 @@ def get_checks(hw: Homework) -> list[Check]:
     :return: checks list
     """
     checks, seen = [], set()
-    for path, path_content in hw.content.items():
-        filename = path.rsplit(os.sep, maxsplit=1)[-1]
-        name, _, suffix = filename.rpartition(".")
-        if name not in seen:
-            if suffix in get_runtime_suffix():
-                content = {}
-                for suf in get_runtime_suffix():
-                    file = f"{name}.{suf}"
-                    content[file] = hw.content.get(path.removesuffix(suffix) + suf, None)
+    for check_path, check_content in hw.content.items():
+        if check_path.startswith(get_check_name()):
+            path_beg, _, suffix = check_path.rpartition(".")
+            name = path_beg.rsplit("/", maxsplit=1)[-1]
+
+            if name not in seen:
+                content, category = {}, None
+                if suffix in get_runtime_suffix():
+                    category = CheckCategoryEnum.runtime
+                    for suf in get_runtime_suffix():
+                        content[f"{name}.{suf}"] = hw.content.get(f"{path_beg}.{suf}", None)
+                elif suffix == get_validate_suffix():
+                    category = CheckCategoryEnum.validate
+                    content = {f"{name}.{suffix}": check_content}
+                else:
+                    continue
+
                 check = Check(
                     content=content,
-                    category=CheckCategoryEnum.runtime,
-                    ID=f"{hw.USER_ID}:{hw.TASK_ID}/{filename}",
+                    category=category,
+                    ID=f"{hw.USER_ID}:{hw.TASK_ID}/{name}",
                     TASK_ID=hw.TASK_ID,
                     USER_ID=hw.USER_ID,
                     timestamp=hw.timestamp,
                 )
-            elif suffix == get_validate_suffix() and filename != get_prog_name():
-                check = Check(
-                    content={filename: path_content},
-                    category=CheckCategoryEnum.validate,
-                    ID=f"{hw.USER_ID}:{hw.TASK_ID}/{filename}",
-                    TASK_ID=hw.TASK_ID,
-                    USER_ID=hw.USER_ID,
-                    timestamp=hw.timestamp,
-                )
-            else:
-                continue
-            seen.add(name)
-            checks.append(check)
+                seen.add(name)
+                checks.append(check)
 
     return checks
 
