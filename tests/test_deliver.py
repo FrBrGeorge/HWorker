c4ab9ef5c83f676267d4489028c6113649a15fff
Nikita Barabanov <Barabanov.ns@gmail.com>
2023-08-16 14:14:19 +0300

    Change path in deliver git to relative, fix tests

--- tests/test_deliver.py
+++ tests/test_deliver.py
@@ -6,6 +6,8 @@ import tempfile
 import pytest
 from git import Repo
 
+from hworker.deliver.git import get_homework_content
+
 
 @pytest.fixture()
 def example_git_repo():
@@ -16,19 +18,18 @@ def example_git_repo():
         repo = Repo.init(repo_path)
         with open(os.path.join(repo_path, "prog.py"), "wb") as p:
             p.write(b"a, b = eval(input())\n" b"print(max(a, b))")
-        with open(os.path.join(repo_path, "URLS"), "wb") as urls:
-            urls.write(b"https://github.com/Test/test/tree/main/20220913/1/tests")
-        test_path = os.path.join(repo_path, "checks")
+        test_path = os.path.join(repo_path, "check")
         os.mkdir(test_path)
         with open(os.path.join(test_path, "1.in"), "wb") as test_in:
             test_in.write(b"123, 345")
         with open(os.path.join(test_path, "1.out"), "wb") as test_out:
             test_out.write(b"345")
+        with open(os.path.join(test_path, "remote"), "wb") as urls:
+            urls.write(b"UserN:TaskN")
         repo.git.add(".")
         repo.git.commit(message="test commit")
 
         yield repo_path
-
 
 
 class TestDeliverGit:
@@ -36,14 +37,10 @@ class TestDeliverGit:
 
     def test_get_homework_content(self, example_git_repo):
         """"""
-        from hworker.deliver.git import get_homework_content
 
-        print(*get_homework_content(example_git_repo).items(), sep="\n")
-
-        # todo should be relative paths from git root. actually without root folder name(test_repo) in this example
         assert get_homework_content(example_git_repo) == {
-            "/test_repo/prog.py": b"a, b = eval(input())\n" b"print(max(a, b))",
-            "/test_repo/URLS": b"https://github.com/Test/test/tree/main/20220913/1/tests",
-            "/test_repo/checks/1.in": b"123, 345",
-            "/test_repo/checks/1.out": b"345",
+            "prog.py": b"a, b = eval(input())\n" b"print(max(a, b))",
+            "check/remote": b"UserN:TaskN",
+            "check/1.in": b"123, 345",
+            "check/1.out": b"345",
         }
