439fb7a80d1a6954ebb0a03a476c37816c81198d
George V. Kouryachy (Fr. Br. George) <george@altlinux.org>
2023-08-29 16:31:56 +0300

    Ignore some names (e. g. __pycache__) in deliver.file

--- hworker/deliver/file/__init__.py
+++ hworker/deliver/file/__init__.py
@@ -1,6 +1,7 @@
 """file backend"""
 import datetime
 import os
+import re
 from pathlib import Path
 
 from ... import depot
@@ -10,6 +11,7 @@ from ...log import get_logger
 
 _default_datetime = datetime.datetime.fromisoformat("2009-05-17 20:09:00")
 _depot_prefix = "f"
+_ignore_pattern = re.compile(r"(.*/|^)__pycache__/.*")
 
 
 def download_all():
@@ -35,7 +37,7 @@ def download_all():
             timestamps = []
             contents = {}
             for file in Path(task_path := user_path + os.sep + task_name).rglob("**/*"):
-                if file.is_file():
+                if file.is_file() and not _ignore_pattern.match(str(file)):
                     with open(file, "rb") as file_in:
                         contents[file.relative_to(task_path).as_posix()] = file_in.read()
                     timestamps.append(os.path.getmtime(file))
