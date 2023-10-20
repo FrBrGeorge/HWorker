f27143d8c008b0761f2b54579d6445ddffb39552
Veniamin Arefev <veniamin.arefev@mail.ru>
2023-10-20 15:55:34 +0300

    Added backend support for student info

--- hworker/publish/app.py
+++ hworker/publish/app.py
@@ -97,6 +97,61 @@ def info():
     return render_template("info.html", username=username, taskname=taskname, tables=tables)
 
 
+# @app.get("/student/<user_id:string>")
+@app.get("/student/<user_id>")
+def student(user_id):
+    data_per_user: dict[str, list] = defaultdict(list)
+
+    final_score_names = list(map(lambda x: x.name, depot.search(depot.objects.Formula)))
+    user_score_names = list(map(lambda x: x.name, depot.search(depot.objects.UserQualifier)))
+    task_score_names = {
+        task_id: list(map(lambda x: x.name, depot.search(depot.objects.TaskQualifier)))
+        for task_id in config.get_tasks_list()
+    }
+
+    for big_names, search_object in zip(
+        [final_score_names, user_score_names, task_score_names],
+        [depot.objects.FinalScore, depot.objects.UserScore, depot.objects.TaskScore],
+    ):
+        if type(big_names) == list:
+            for name in big_names:
+                score = depot.search(
+                    search_object,
+                    depot.objects.Criteria("USER_ID", "==", user_id),
+                    depot.objects.Criteria("name", "==", name),
+                    first=True,
+                )
+                data_per_user[user_id].append(None if score is None else score.rating)
+        else:
+            for task_id, names in big_names.items():
+                for name in names:
+                    score = depot.search(
+                        search_object,
+                        depot.objects.Criteria("TASK_ID", "==", task_id),
+                        depot.objects.Criteria("USER_ID", "==", user_id),
+                        depot.objects.Criteria("name", "==", name),
+                        first=True,
+                    )
+                    data_per_user[user_id].append(None if score is None else score.rating)
+
+    header = list()
+    header.append("Users")
+    if len(final_score_names) != 0:
+        header.append(final_score_names[0])
+    type_pretty_names = ["User Qualifiers", "Task Qualifiers"]
+    for type_index, big_names in enumerate([user_score_names, task_score_names]):
+        header.append({type_pretty_names[type_index]: big_names})
+
+    rows = [[key] + value for key, value in data_per_user.items()]
+
+    return render_template(
+        "index.html",
+        table=create_table(header, rows),
+        skip_prefix=len(final_score_names) + len(user_score_names) + 1,
+        homework_names=",".join(config.get_tasks_list()),
+    )
+
+
 @app.get("/status")
 def status():
     data: list[depot.objects.UpdateTime] = list(depot.search(depot.objects.UpdateTime))
